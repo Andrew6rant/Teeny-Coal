f57fcb1c9d82ec8b9b1484f524d67faf57a6bd94
Andrew6rant <57331134+Andrew6rant@users.noreply.github.com>
2021-07-31 15:17:47 -0400

    simplify torch registration

--- src/main/java/io/github/Andrew6rant/teenycoal/TeenyCoal.java
+++ src/main/java/io/github/Andrew6rant/teenycoal/TeenyCoal.java
@@ -4,15 +4,12 @@ import io.github.Andrew6rant.teenycoal.block.TeenyTorch;
 import io.github.Andrew6rant.teenycoal.block.TeenyWallTorch;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
-import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.fabricmc.fabric.api.registry.FuelRegistry;
 import net.minecraft.block.Block;
-import net.minecraft.block.Blocks;
 import net.minecraft.block.Material;
 import net.minecraft.item.*;
 import net.minecraft.particle.ParticleTypes;
-import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
@@ -25,21 +22,26 @@ public class TeenyCoal implements ModInitializer {
 	public static void registerItem(String itemName, Item item) {
 		Registry.register(Registry.ITEM, new Identifier("teenycoal", itemName), item);
 	}
+	public static void registerTorch(String torchName, Block torch, String wallTorchName, Block wallTorch) {
+		Registry.register(Registry.BLOCK, new Identifier("teenycoal", torchName), torch);
+		Registry.register(Registry.BLOCK, new Identifier("teenycoal", wallTorchName), wallTorch);
+		Registry.register(Registry.ITEM, new Identifier("teenycoal", torchName), new WallStandingBlockItem(torch, wallTorch, new Item.Settings().group(TeenyCoal.ITEM_GROUP)));
+	}
 	public static void registerFuel(Object blockOrItem, int value) {
 		FuelRegistry.INSTANCE.add((ItemConvertible) blockOrItem, value);
 	}
 
-	public static final TeenyTorch TEENY_TORCH = new TeenyTorch(FabricBlockSettings.of(Material.DECORATION).nonOpaque().luminance(10), ParticleTypes.FLAME);
-	public static final TeenyWallTorch TEENY_WALL_TORCH = new TeenyWallTorch(FabricBlockSettings.of(Material.DECORATION).nonOpaque().luminance(10), ParticleTypes.FLAME);
+	public static final TeenyTorch TEENY_TORCH = new TeenyTorch(FabricBlockSettings.of(Material.DECORATION).nonOpaque().noCollision().luminance(10), ParticleTypes.FLAME);
+	public static final TeenyWallTorch TEENY_WALL_TORCH = new TeenyWallTorch(FabricBlockSettings.of(Material.DECORATION).nonOpaque().noCollision().luminance(10), ParticleTypes.FLAME);
+	public static final TeenyTorch TEENY_REDSTONE_TORCH = new TeenyTorch(FabricBlockSettings.of(Material.DECORATION).nonOpaque().luminance(10), ParticleTypes.FLAME);
+	public static final TeenyWallTorch TEENY_REDSTONE_WALL_TORCH = new TeenyWallTorch(FabricBlockSettings.of(Material.DECORATION).nonOpaque().luminance(10), ParticleTypes.FLAME);
 	public static final Item TEENY_COAL = new Item(new Item.Settings().group(TeenyCoal.ITEM_GROUP));
 	public static final Item TEENY_CHARCOAL = new Item(new Item.Settings().group(TeenyCoal.ITEM_GROUP));
 
 	@Override
 	public void onInitialize() {
-		Registry.register(Registry.BLOCK, new Identifier("teenycoal", "teeny_torch"), TEENY_TORCH);
-		Registry.register(Registry.BLOCK, new Identifier("teenycoal", "teeny_wall_torch"), TEENY_WALL_TORCH);
-		Registry.register(Registry.ITEM, new Identifier("teenycoal", "teeny_torch"), new WallStandingBlockItem(TEENY_TORCH, TEENY_WALL_TORCH, new Item.Settings().group(TeenyCoal.ITEM_GROUP)));
+		registerTorch(Names.TEENY_TORCH, TEENY_TORCH, Names.TEENY_WALL_TORCH, TEENY_WALL_TORCH);
 		registerItem(Names.TEENY_CHARCOAL, TEENY_CHARCOAL);	registerFuel(TEENY_CHARCOAL, 10);
-		registerItem(Names.TEENY_COAL, TEENY_COAL);			registerFuel(TEENY_COAL, 10);
+		registerItem(Names.TEENY_COAL, TEENY_COAL);	registerFuel(TEENY_COAL, 10);
 	}
 }
