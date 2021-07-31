5a03b28439038f0c5dc2800b99cc608bf332abd5
Andrew6rant <57331134+Andrew6rant@users.noreply.github.com>
2021-07-31 11:15:54 -0400

    Initial commit

---
+++ src/main/java/io/github/Andrew6rant/teenycoal/TeenyCoal.java
@@ -0,0 +1,14 @@
+package io.github.Andrew6rant.teenycoal;
+
+import net.fabricmc.api.ModInitializer;
+
+public class TeenyCoal implements ModInitializer {
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+
+		System.out.println("Hello Fabric world!");
+	}
+}
