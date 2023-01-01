6946c3eef5c08009fa7dd42643f01678b9a65245
Andrew Grant <andrew.grant@mail.rit.edu>
2023-01-01 17:03:24 -0500

    port to 1.19.3

--- src/main/java/io/github/Andrew6rant/teenycoal/TeenyCoal.java
+++ src/main/java/io/github/Andrew6rant/teenycoal/TeenyCoal.java
@@ -5,29 +5,44 @@ import io.github.Andrew6rant.teenycoal.block.TeenyRedstoneWallTorch;
 import io.github.Andrew6rant.teenycoal.block.TeenyTorch;
 import io.github.Andrew6rant.teenycoal.block.TeenyWallTorch;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.fabricmc.fabric.api.registry.FuelRegistry;
 import net.minecraft.block.Block;
 import net.minecraft.block.Material;
 import net.minecraft.item.*;
 import net.minecraft.particle.ParticleTypes;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
+import net.minecraft.util.math.Direction;
 
 public class TeenyCoal implements ModInitializer {
 
-	public static final ItemGroup ITEM_GROUP = FabricItemGroupBuilder.build(
-			new Identifier("teenycoal", "general"),
-			() -> new ItemStack(TeenyCoal.TEENY_TORCH));
+	public static final ItemGroup ITEM_GROUP = FabricItemGroup.builder(new Identifier("teenycoal", "general"))
+			//.displayName(Text.literal("Example Item Group"))
+			.icon(() -> new ItemStack(TeenyCoal.TEENY_TORCH))
+			//.entries((enabledFeatures, entries, operatorEnabled) -> {
+			//	entries.add(Items.DIAMOND);
+			//})
+			.build();
+
+	//public static final ItemGroup ITEM_GROUP = FabricItemGroupBuilder.build(
+	//		new Identifier("teenycoal", "general"),
+	//		() -> new ItemStack(TeenyCoal.TEENY_TORCH));
 
 	public static void registerItem(String itemName, Item item) {
-		Registry.register(Registry.ITEM, new Identifier("teenycoal", itemName), item);
+		Registry.register(Registries.ITEM, new Identifier("teenycoal", itemName), item);
+		ItemGroupEvents.modifyEntriesEvent(TeenyCoal.ITEM_GROUP).register(entries -> entries.add(item));
 	}
 	public static void registerTorch(String torchName, Block torch, String wallTorchName, Block wallTorch) {
-		Registry.register(Registry.BLOCK, new Identifier("teenycoal", torchName), torch);
-		Registry.register(Registry.BLOCK, new Identifier("teenycoal", wallTorchName), wallTorch);
-		Registry.register(Registry.ITEM, new Identifier("teenycoal", torchName), new WallStandingBlockItem(torch, wallTorch, new Item.Settings().group(TeenyCoal.ITEM_GROUP)));
+		Registry.register(Registries.BLOCK, new Identifier("teenycoal", torchName), torch);
+		Registry.register(Registries.BLOCK, new Identifier("teenycoal", wallTorchName), wallTorch);
+		Registry.register(Registries.ITEM, new Identifier("teenycoal", torchName), new VerticallyAttachableBlockItem(torch, wallTorch, new FabricItemSettings(), Direction.DOWN));
+		ItemGroupEvents.modifyEntriesEvent(TeenyCoal.ITEM_GROUP).register(entries -> entries.add(torch));
 	}
 	public static void registerFuel(Item item, int value) {
 		FuelRegistry.INSTANCE.add(item, value);
@@ -39,11 +54,11 @@ public class TeenyCoal implements ModInitializer {
 	public static final TeenyRedstoneWallTorch TEENY_REDSTONE_WALL_TORCH = new TeenyRedstoneWallTorch(FabricBlockSettings.of(Material.DECORATION).nonOpaque().noCollision().luminance(4));
 	public static final TeenyTorch TEENY_SOUL_TORCH = new TeenyTorch(FabricBlockSettings.of(Material.DECORATION).nonOpaque().noCollision().luminance(8), ParticleTypes.SOUL_FIRE_FLAME);
 	public static final TeenyWallTorch TEENY_SOUL_WALL_TORCH = new TeenyWallTorch(FabricBlockSettings.of(Material.DECORATION).nonOpaque().noCollision().luminance(8), ParticleTypes.SOUL_FIRE_FLAME);
-	public static final Item TEENY_COAL = new Item(new Item.Settings().group(TeenyCoal.ITEM_GROUP));
-	public static final Item TEENY_CHARCOAL = new Item(new Item.Settings().group(TeenyCoal.ITEM_GROUP));
-	public static final Item TEENY_STICK = new Item(new Item.Settings().group(TeenyCoal.ITEM_GROUP));
-	public static final Item SOUL_DUST = new Item(new Item.Settings().group(TeenyCoal.ITEM_GROUP));
-	public static final Item TEENY_REDSTONE_DUST = new Item(new Item.Settings().group(TeenyCoal.ITEM_GROUP));
+	public static final Item TEENY_COAL = new Item(new Item.Settings());
+	public static final Item TEENY_CHARCOAL = new Item(new Item.Settings());
+	public static final Item TEENY_STICK = new Item(new Item.Settings());
+	public static final Item SOUL_DUST = new Item(new Item.Settings());
+	public static final Item TEENY_REDSTONE_DUST = new Item(new Item.Settings());
 
 	@Override
 	public void onInitialize() {
