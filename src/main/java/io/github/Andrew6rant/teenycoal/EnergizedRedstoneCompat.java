ae5f37b128c2d2080a5698be4695eb21a84fdad7
Andrew Grant <andrew.grant@mail.rit.edu>
2023-06-11 00:22:39 -0400

    port to 1.20 and update release script

--- src/main/java/io/github/Andrew6rant/teenycoal/EnergizedRedstoneCompat.java
+++ src/main/java/io/github/Andrew6rant/teenycoal/EnergizedRedstoneCompat.java
@@ -6,7 +6,7 @@ import io.github.Andrew6rant.teenycoal.compat.TeenyEnergizedRedstoneTorch;
 import io.github.Andrew6rant.teenycoal.compat.TeenyEnergizedRedstoneWallTorch;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
-import net.minecraft.block.Material;
+import net.minecraft.block.Blocks;
 import net.minecraft.client.render.RenderLayer;
 import net.minecraft.item.Item;
 
@@ -15,8 +15,8 @@ import static io.github.Andrew6rant.teenycoal.TeenyCoal.registerTorch;
 
 public class EnergizedRedstoneCompat {
 
-    public static final TeenyRedstoneTorch TEENY_ENERGIZED_REDSTONE_TORCH = new TeenyEnergizedRedstoneTorch(FabricBlockSettings.of(Material.DECORATION).nonOpaque().noCollision().luminance(10));
-    public static final TeenyRedstoneWallTorch TEENY_ENERGIZED_REDSTONE_WALL_TORCH = new TeenyEnergizedRedstoneWallTorch(FabricBlockSettings.of(Material.DECORATION).nonOpaque().noCollision().luminance(10));
+    public static final TeenyRedstoneTorch TEENY_ENERGIZED_REDSTONE_TORCH = new TeenyEnergizedRedstoneTorch(FabricBlockSettings.copyOf(Blocks.REDSTONE_TORCH).nonOpaque().noCollision().luminance(10));
+    public static final TeenyRedstoneWallTorch TEENY_ENERGIZED_REDSTONE_WALL_TORCH = new TeenyEnergizedRedstoneWallTorch(FabricBlockSettings.copyOf(Blocks.REDSTONE_WALL_TORCH).nonOpaque().noCollision().luminance(10));
     public static final Item TEENY_ENERGIZED_REDSTONE_DUST = new Item(new Item.Settings());
 
     public static void energizedRedstoneCompat() {
--- src/main/java/io/github/Andrew6rant/teenycoal/TeenyCoal.java
+++ src/main/java/io/github/Andrew6rant/teenycoal/TeenyCoal.java
@@ -12,11 +12,14 @@ import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.fabricmc.fabric.api.registry.FuelRegistry;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.block.Block;
-import net.minecraft.block.Material;
+import net.minecraft.block.Blocks;
 import net.minecraft.item.*;
 import net.minecraft.particle.ParticleTypes;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.Direction;
 
@@ -24,9 +27,7 @@ import static io.github.Andrew6rant.teenycoal.EnergizedRedstoneCompat.energizedR
 
 public class TeenyCoal implements ModInitializer {
 
-	public static final ItemGroup ITEM_GROUP = FabricItemGroup.builder(new Identifier("teenycoal", "general"))
-			.icon(() -> new ItemStack(TeenyCoal.TEENY_TORCH))
-			.build();
+	private static final RegistryKey<ItemGroup> ITEM_GROUP = RegistryKey.of(RegistryKeys.ITEM_GROUP, new Identifier("teenycoal", "general"));
 
 
 	// Namespace would normally be hardcoded to "teenycoal", but I need to register under different namespaces for mod compatibility
@@ -45,12 +46,12 @@ public class TeenyCoal implements ModInitializer {
 		FuelRegistry.INSTANCE.add(item, value);
 	}
 
-	public static final TeenyTorch TEENY_TORCH = new TeenyTorch(FabricBlockSettings.of(Material.DECORATION).nonOpaque().noCollision().luminance(10), ParticleTypes.FLAME);
-	public static final TeenyWallTorch TEENY_WALL_TORCH = new TeenyWallTorch(FabricBlockSettings.of(Material.DECORATION).nonOpaque().noCollision().luminance(10), ParticleTypes.FLAME);
-	public static final TeenyRedstoneTorch TEENY_REDSTONE_TORCH = new TeenyRedstoneTorch(FabricBlockSettings.of(Material.DECORATION).nonOpaque().noCollision().luminance(4));
-	public static final TeenyRedstoneWallTorch TEENY_REDSTONE_WALL_TORCH = new TeenyRedstoneWallTorch(FabricBlockSettings.of(Material.DECORATION).nonOpaque().noCollision().luminance(4));
-	public static final TeenyTorch TEENY_SOUL_TORCH = new TeenyTorch(FabricBlockSettings.of(Material.DECORATION).nonOpaque().noCollision().luminance(8), ParticleTypes.SOUL_FIRE_FLAME);
-	public static final TeenyWallTorch TEENY_SOUL_WALL_TORCH = new TeenyWallTorch(FabricBlockSettings.of(Material.DECORATION).nonOpaque().noCollision().luminance(8), ParticleTypes.SOUL_FIRE_FLAME);
+	public static final TeenyTorch TEENY_TORCH = new TeenyTorch(FabricBlockSettings.copyOf(Blocks.TORCH).nonOpaque().noCollision().luminance(10), ParticleTypes.FLAME);
+	public static final TeenyWallTorch TEENY_WALL_TORCH = new TeenyWallTorch(FabricBlockSettings.copyOf(Blocks.WALL_TORCH).nonOpaque().noCollision().luminance(10), ParticleTypes.FLAME);
+	public static final TeenyRedstoneTorch TEENY_REDSTONE_TORCH = new TeenyRedstoneTorch(FabricBlockSettings.copyOf(Blocks.REDSTONE_TORCH).nonOpaque().noCollision().luminance(4));
+	public static final TeenyRedstoneWallTorch TEENY_REDSTONE_WALL_TORCH = new TeenyRedstoneWallTorch(FabricBlockSettings.copyOf(Blocks.REDSTONE_WALL_TORCH).nonOpaque().noCollision().luminance(4));
+	public static final TeenyTorch TEENY_SOUL_TORCH = new TeenyTorch(FabricBlockSettings.copyOf(Blocks.SOUL_TORCH).nonOpaque().noCollision().luminance(8), ParticleTypes.SOUL_FIRE_FLAME);
+	public static final TeenyWallTorch TEENY_SOUL_WALL_TORCH = new TeenyWallTorch(FabricBlockSettings.copyOf(Blocks.SOUL_WALL_TORCH).nonOpaque().noCollision().luminance(8), ParticleTypes.SOUL_FIRE_FLAME);
 	public static final Item TEENY_COAL = new Item(new Item.Settings());
 	public static final Item TEENY_CHARCOAL = new Item(new Item.Settings());
 	public static final Item TEENY_STICK = new Item(new Item.Settings());
@@ -62,11 +63,21 @@ public class TeenyCoal implements ModInitializer {
 		registerTorch(Names.NAMESPACE, Names.TEENY_TORCH, TEENY_TORCH, Names.TEENY_WALL_TORCH, TEENY_WALL_TORCH);
 		registerTorch(Names.NAMESPACE, Names.TEENY_REDSTONE_TORCH, TEENY_REDSTONE_TORCH, Names.TEENY_REDSTONE_WALL_TORCH, TEENY_REDSTONE_WALL_TORCH);
 		registerTorch(Names.NAMESPACE, Names.TEENY_SOUL_TORCH, TEENY_SOUL_TORCH, Names.TEENY_SOUL_WALL_TORCH, TEENY_SOUL_WALL_TORCH);
-		registerItem(Names.NAMESPACE, Names.TEENY_CHARCOAL, TEENY_CHARCOAL);	registerFuel(TEENY_CHARCOAL, 200);
-		registerItem(Names.NAMESPACE, Names.TEENY_COAL, TEENY_COAL);			registerFuel(TEENY_COAL, 200);
-		registerItem(Names.NAMESPACE, Names.TEENY_STICK, TEENY_STICK);		registerFuel(TEENY_STICK, 15);
+		registerItem(Names.NAMESPACE, Names.TEENY_CHARCOAL, TEENY_CHARCOAL);
+		registerItem(Names.NAMESPACE, Names.TEENY_COAL, TEENY_COAL);
+		registerItem(Names.NAMESPACE, Names.TEENY_STICK, TEENY_STICK);
 		registerItem(Names.NAMESPACE, Names.SOUL_DUST, SOUL_DUST);
 		registerItem(Names.NAMESPACE, Names.TEENY_REDSTONE_DUST, TEENY_REDSTONE_DUST);
+
+		registerFuel(TEENY_CHARCOAL, 200);
+		registerFuel(TEENY_COAL, 200);
+		registerFuel(TEENY_STICK, 15);
+
+		Registry.register(Registries.ITEM_GROUP, ITEM_GROUP, FabricItemGroup.builder()
+				.displayName(Text.translatable("itemGroup.teenycoal.general"))
+				.icon(() -> new ItemStack(TEENY_TORCH))
+				.build()
+		);
 
 		if (FabricLoader.getInstance().isModLoaded("energized_redstone")) {
 			energizedRedstoneCompat();
